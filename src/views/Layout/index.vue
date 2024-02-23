aea9f2ae6a007328722cf28ea676adf9a770e805
Furong-Ou <3335219671@qq.com>
2024-02-23 18:17:17 +0800

    静态资源搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,4 +1,13 @@
+
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    <div>这里是首页</div>
-    <RouterView />
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
