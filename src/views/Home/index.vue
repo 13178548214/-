0c7216b6e8157a4e95d7fda22631c8f3ed4173c0
Furong-Ou <3335219671@qq.com>
2024-03-04 21:46:49 +0800

    home页面的分类

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,17 @@
-<template>
-    <div style="height: 300px;">
-     这里是Home    
-    </div>
-</template>
-
-<script lang="ts" setup>
-
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
 </script>
+
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
+</template>
