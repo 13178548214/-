146a8a60103c0e1c23596693c9eb97e1f9867ba9
Furong-Ou <3335219671@qq.com>
2024-03-11 21:33:08 +0800

    会员中心-整体路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,10 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import payBack from '@/views/Pay/components/payBack.vue'
+import Member from '@/views/Member/index.vue'
+import User from '@/views/Member/components/UserInfo.vue'
+import Order from '@/views/Member/components/OrderInfo.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -47,7 +51,21 @@ const router = createRouter({
       {
         path:'/paycallback',
         component:payBack
-      }
+      },
+      {
+        path: '/member',
+        component: Member,
+        children:[
+          {
+            path:'user',
+            component:User
+          },
+          {
+            path:'order',
+            component:Order
+          }
+        ]
+    }
       ]
     },
     {
