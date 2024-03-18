f7e9ad8f626dd6372daadaff299ac82f7dda67f0
Furong-Ou <3335219671@qq.com>
2024-03-18 16:03:27 +0800

    前台分类-组件封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,6 +5,7 @@ import { getHomeBannerAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
 
 const homeBannerList = ref<BannerItem[]>([])
 const getHomeBanner = async () => {
@@ -21,7 +22,12 @@ onLoad(() => {
 <template>
   <CustomNavbar />
   <XtxSwiper :list="homeBannerList" />
+  <CategoryPanel />
   <view class="index"> </view>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+}
+</style>
