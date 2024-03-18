67edc3c3e32e3301e934c75ddc390e7b455bff07
Furong-Ou <3335219671@qq.com>
2024-03-18 14:45:40 +0800

    轮播图:指示点

--- src/components/XtxSwiper.vue
+++ src/components/XtxSwiper.vue
@@ -2,11 +2,16 @@
 import { ref } from 'vue'
 
 const activeIndex = ref(0)
+//当swiper 小标发生改变时触发
+const onChange: UniHelper.SwiperOnChange = (e) => {
+  //! 非空断言，即一定不会出现空集的情况
+  activeIndex.value = e.detail!.current
+}
 </script>
 
 <template>
   <view class="carousel">
-    <swiper :circular="true" :autoplay="false" :interval="3000">
+    <swiper :circular="true" :autoplay="false" :interval="3000" @change="onChange">
       <swiper-item>
         <navigator url="/pages/index/index" hover-class="none" class="navigator">
           <image
