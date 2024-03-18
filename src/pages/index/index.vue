8877211515d4907bb8ee1165bf5c9f5a2933bdb7
Furong-Ou <3335219671@qq.com>
2024-03-18 15:40:58 +0800

    轮播图-数据类型定义和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 import XtxSwiper from '@/components/XtxSwiper.vue'
 import CustomNavbar from './components/CustomNavbar.vue'
+import { getHomeBannerAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem } from '@/types/home'
+
+const homeBannerList = ref<BannerItem[]>([])
+const getHomeBanner = async () => {
+  const res = await getHomeBannerAPI()
+  homeBannerList.value = res.result
+  console.log(homeBannerList.value)
+}
+
+onLoad(() => {
+  getHomeBanner()
+})
 </script>
 
 <template>
   <CustomNavbar />
-  <XtxSwiper />
+  <XtxSwiper :list="homeBannerList" />
   <view class="index"> </view>
 </template>
 
