6fd264956bd08e07920117c8c91f60d570de62a1
Furong-Ou <3335219671@qq.com>
2024-03-19 20:44:25 +0800

    热门推荐-类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,8 +1,9 @@
 import type { PageParams } from '@/types/global'
+import type { HotItem } from '@/types/home'
 import { http } from '@/utils/http'
 
 export const getHotRecommentAPI = (url: string, data?: PageParams & { subType: string }) => {
-  return http({
+  return http<HotItem>({
     method: 'GET',
     url,
     data,
