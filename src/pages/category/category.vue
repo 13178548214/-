abded51dc71a44cc9ae08f3ef656981a58a38c73
Furong-Ou <3335219671@qq.com>
2024-03-20 15:11:21 +0800

    商品分类-骨架屏

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -5,6 +5,7 @@ import type { BannerItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { computed, ref } from 'vue'
 import type { CategoryTopItem } from '@/types/category'
+import pageSketoken from './components/pageSketoken.vue'
 const bannerList = ref<BannerItem[]>([])
 
 //获取轮播图数据
@@ -28,65 +29,70 @@ const subCategoryList = computed(() => {
   return categoryTopList.value[activeIndex.value]?.children || []
 })
 
-onLoad(() => {
-  getCategoryTop(), getBannerData()
+//判断是否加载完成
+const isLoding = ref(false)
+onLoad(async () => {
+  await Promise.all([getCategoryTop(), getBannerData()])
+  isLoding.value = true
 })
 </script>
 
 <template>
-  <view class="viewport">
-    <!-- 搜索框 -->
-    <view class="search">
-      <view class="input">
-        <text class="icon-search">女靴</text>
+  <template v-if="isLoding">
+    <view class="viewport">
+      <!-- 搜索框 -->
+      <view class="search">
+        <view class="input">
+          <text class="icon-search">女靴</text>
+        </view>
+      </view>
+      <!-- 分类 -->
+      <view class="categories">
+        <!-- 左侧：一级分类 -->
+        <scroll-view class="primary" scroll-y>
+          <view
+            v-for="(item, index) in categoryTopList"
+            :key="item.id"
+            class="item"
+            :class="{ active: index === activeIndex }"
+            @tap="activeIndex = index"
+          >
+            <text class="name"> {{ item.name }} </text>
+          </view>
+        </scroll-view>
+        <!-- 右侧：二级分类 -->
+        <scroll-view class="secondary" scroll-y>
+          <!-- 焦点图 -->
+          <XtxSwiper class="banner" :list="bannerList" />
+          <!-- 内容区域 -->
+          <view class="panel" v-for="item in subCategoryList" :key="item.id">
+            <view class="title">
+              <text class="name">{{ item.name }}</text>
+              <navigator class="more" hover-class="none">全部</navigator>
+            </view>
+            <view class="section">
+              <navigator
+                v-for="goods in item.goods"
+                :key="goods.id"
+                class="goods"
+                hover-class="none"
+                :url="`/pages/goods/goods?id=${goods.id}`"
+              >
+                <image class="image" :src="goods.picture"></image>
+                <view class="name ellipsis">{{ goods.name }}</view>
+                <view class="price">
+                  <text class="symbol">¥</text>
+                  <text class="number">{{ goods.price }}</text>
+                </view>
+              </navigator>
+            </view>
+          </view>
+        </scroll-view>
       </view>
     </view>
-    <!-- 分类 -->
-    <view class="categories">
-      <!-- 左侧：一级分类 -->
-      <scroll-view class="primary" scroll-y>
-        <view
-          v-for="(item, index) in categoryTopList"
-          :key="item.id"
-          class="item"
-          :class="{ active: index === activeIndex }"
-          @tap="activeIndex = index"
-        >
-          <text class="name"> {{ item.name }} </text>
-        </view>
-      </scroll-view>
-      <!-- 右侧：二级分类 -->
-      <scroll-view class="secondary" scroll-y>
-        <!-- 焦点图 -->
-        <XtxSwiper class="banner" :list="bannerList" />
-        <!-- 内容区域 -->
-        <view class="panel" v-for="item in subCategoryList" :key="item.id">
-          <view class="title">
-            <text class="name">{{ item.name }}</text>
-            <navigator class="more" hover-class="none">全部</navigator>
-          </view>
-          <view class="section">
-            <navigator
-              v-for="goods in item.goods"
-              :key="goods.id"
-              class="goods"
-              hover-class="none"
-              :url="`/pages/goods/goods?id=${goods.id}`"
-            >
-              <image class="image" :src="goods.picture"></image>
-              <view class="name ellipsis">{{ goods.name }}</view>
-              <view class="price">
-                <text class="symbol">¥</text>
-                <text class="number">{{ goods.price }}</text>
-              </view>
-            </navigator>
-          </view>
-        </view>
-      </scroll-view>
-    </view>
-  </view>
+  </template>
+  <pageSketoken v-else />
 </template>
-
 <style lang="scss">
 page {
   height: 100%;
---
+++ src/pages/category/components/pageSketoken.vue
@@ -0,0 +1,447 @@
+<script setup></script>
+
+<template name="skeleton">
+  <view class="sk-container">
+    <view class="viewport viewport">
+      <view class="search search">
+        <view class="input input">
+          <text
+            class="icon-search sk-transparent sk-text-14-2857-289 sk-text sk-pseudo sk-pseudo-circle"
+            >女靴</text
+          >
+        </view>
+      </view>
+      <view class="categories categories">
+        <scroll-view :scroll-y="true" class="primary primary">
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-586 sk-text">居家</text>
+          </view>
+          <view class="item active sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-223 sk-text">美食</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-643 sk-text">服饰</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-708 sk-text">母婴</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-666 sk-text">个护</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-144 sk-text">严选</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-980 sk-text">数码</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-342 sk-text">运动</text>
+          </view>
+          <view class="item sk-pseudo sk-pseudo-circle">
+            <text class="name sk-transparent sk-text-14-2857-71 sk-text">杂项</text>
+          </view>
+        </scroll-view>
+        <scroll-view :scroll-y="true" class="secondary secondary">
+          <view is="components/XtxSwiper" class="banner banner">
+            <view class="carousel XtxSwiper--carousel">
+              <swiper :circular="true" :interval="3000" :current="0" :autoplay="false">
+                <swiper-item
+                  style="
+                    position: absolute;
+                    width: 100%;
+                    height: 100%;
+                    transform: translate(0%, 0px) translateZ(0px);
+                  "
+                >
+                  <navigator class="navigator XtxSwiper--navigator" hover-class="none">
+                    <image class="image XtxSwiper--image sk-image" mode="aspectFill"></image>
+                  </navigator>
+                </swiper-item>
+                <swiper-item
+                  style="
+                    position: absolute;
+                    width: 100%;
+                    height: 100%;
+                    transform: translate(100%, 0px) translateZ(0px);
+                  "
+                >
+                  <navigator class="navigator XtxSwiper--navigator" hover-class="none">
+                    <image class="image XtxSwiper--image sk-image" mode="aspectFill"></image>
+                  </navigator>
+                </swiper-item>
+              </swiper>
+              <view class="indicator XtxSwiper--indicator">
+                <text class="dot XtxSwiper--dot active XtxSwiper--active"></text>
+                <text class="dot XtxSwiper--dot"></text>
+                <text class="dot XtxSwiper--dot"></text>
+                <text class="dot XtxSwiper--dot"></text>
+                <text class="dot XtxSwiper--dot"></text>
+              </view>
+            </view>
+          </view>
+          <view class="panel panel">
+            <view class="title title">
+              <text class="name sk-transparent sk-text-28-0000-954 sk-text">南北干货</text>
+              <navigator
+                class="more sk-transparent sk-text-30-0000-483 sk-text sk-pseudo sk-pseudo-circle"
+                hover-class="none"
+                >全部</navigator
+              >
+            </view>
+            <view class="section section">
+              <navigator class="goods goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-304 sk-text"
+                  >产自核心产区宁夏中宁，枸杞258克</view
+                >
+                <view class="price price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-285 sk-text">36.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-369 sk-text"
+                  >楼兰灰枣328克</view
+                >
+                <view class="price price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-407 sk-text">26.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-65 sk-text"
+                  >莆田桂圆肉368克</view
+                >
+                <view class="price price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-376 sk-text">52.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-235 sk-text"
+                  >古田糯耳150克</view
+                >
+                <view class="price price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-16 sk-text">26.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-319 sk-text"
+                  >无芯软糯的萌妹子，建宁莲子228克</view
+                >
+                <view class="price price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-79 sk-text">45.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods goods" hover-class="none">
+                <image class="image sk-image"></image>
+                <view class="name ellipsis sk-transparent sk-text-14-2857-282 sk-text"
+                  >竹荪25克</view
+                >
+                <view class="price price">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="number sk-transparent sk-text-14-2857-120 sk-text">28.00</text>
+                </view>
+              </navigator>
+              <navigator class="goods goods" hover-class="none">
+                <image class="image sk-image"></image>
+              </navigator>
+              <navigator class="goods goods" hover-class="none">
+                <image class="image sk-image"></image>
+              </navigator>
+            </view>
+          </view>
+        </scroll-view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<style>
+.sk-transparent {
+  color: transparent !important;
+}
+.sk-text-14-2857-289 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text {
+  background-origin: content-box !important;
+  background-clip: content-box !important;
+  background-color: transparent !important;
+  color: transparent !important;
+  background-repeat: repeat-y !important;
+}
+.sk-text-14-2857-586 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-223 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-643 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-708 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-666 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-144 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-980 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-342 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-71 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.0938rpx;
+  position: relative !important;
+}
+.sk-text-28-0000-954 {
+  background-image: linear-gradient(
+    transparent 28%,
+    #eeeeee 0%,
+    #eeeeee 72%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 58.5938rpx;
+  position: relative !important;
+}
+.sk-text-30-0000-483 {
+  background-image: linear-gradient(
+    transparent 30%,
+    #eeeeee 0%,
+    #eeeeee 70%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 58.5938rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-304 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 29.5313rpx;
+  position: relative !important;
+}
+.sk-opacity {
+  opacity: 0 !important;
+}
+.sk-text-14-2857-285 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 32.8125rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-369 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 29.5313rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-407 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 32.8125rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-65 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 29.5313rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-376 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 32.8125rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-235 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 29.5313rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-16 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 32.8125rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-319 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 29.5313rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-79 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 32.8125rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-282 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 29.5313rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-120 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 32.8125rpx;
+  position: relative !important;
+}
+.sk-image {
+  background: #efefef !important;
+}
+.sk-pseudo::before,
+.sk-pseudo::after {
+  background: #efefef !important;
+  background-image: none !important;
+  color: transparent !important;
+  border-color: transparent !important;
+}
+.sk-pseudo-rect::before,
+.sk-pseudo-rect::after {
+  border-radius: 0 !important;
+}
+.sk-pseudo-circle::before,
+.sk-pseudo-circle::after {
+  border-radius: 50% !important;
+}
+.sk-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: transparent;
+}
+</style>
