61edb70c2f71d237325f7f274c0aa9d992ba1c12
Furong-Ou <3335219671@qq.com>
2024-03-23 21:53:49 +0800

    地址模块-完结

--- src/pagesMember/address/address.vue
+++ src/pagesMember/address/address.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { getMemberAddressAPI } from '@/services/address'
+import { deleteMemberAddressByIdAPI, getMemberAddressAPI } from '@/services/address'
 import type { AddressItem } from '@/types/address'
 import { onShow } from '@dcloudio/uni-app'
 import { ref } from 'vue'
@@ -8,6 +8,21 @@ const addressList = ref<AddressItem[]>([])
 const getMemberAddress = async () => {
   const res = await getMemberAddressAPI()
   addressList.value = res.result
+}
+
+//点击删除键
+const deleteMemberAddress = async (id: string) => {
+  uni.showModal({
+    content: '确认删除？',
+    success: async (res) => {
+      if (res.confirm) {
+        //上传要删除的数据的id
+        await deleteMemberAddressByIdAPI(id)
+        //重新渲染数据
+        getMemberAddress()
+      }
+    },
+  })
 }
 
 onShow(() => {
@@ -19,10 +34,10 @@ onShow(() => {
   <view class="viewport">
     <!-- 地址列表 -->
     <scroll-view class="scroll-view" scroll-y>
-      <view v-if="true" class="address">
-        <view class="address-list">
+      <view v-if="addressList.length" class="address">
+        <uni-swipe-action class="address-list">
           <!-- 收货地址项 -->
-          <view class="item" v-for="item in addressList" :key="item.id">
+          <uni-swipe-action-item class="item" v-for="item in addressList" :key="item.id">
             <view class="item-content">
               <view class="user">
                 {{ item.receiver }}
@@ -38,8 +53,12 @@ onShow(() => {
                 修改
               </navigator>
             </view>
-          </view>
-        </view>
+            <!-- 右侧插槽 -->
+            <template #right>
+              <button class="delete-button" @tap="deleteMemberAddress(item.id)">删除</button>
+            </template>
+          </uni-swipe-action-item>
+        </uni-swipe-action>
       </view>
       <view v-else class="blank">暂无收货地址</view>
     </scroll-view>
